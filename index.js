052e4d5513287e854be874c479fbe344f9df90ff
stefan judis <stefanjudis@gmail.com>
2014-10-25 14:02:15 +0200

    added first structure

---
+++ index.js
@@ -0,0 +1,55 @@
+var Runner = require( './lib/runner' );
+var chalk  = require( 'chalk' );
+var debug  = require( 'debug' )( 'bouncer' );
+
+var Bouncer = function( options ) {
+  debug( 'Initialized Bouncer' );
+
+  this.options = {
+    runner : {
+      url            : options.url,
+      allowedDomains : options.allowedDomains
+    },
+    wpt : {
+      pollResults : 20,
+      requests    : true,
+      runs        : options.runs || 1,
+      server      : options.server || 'www.webpagetest.org',
+      timeout     : 480
+    }
+  }
+}
+
+Bouncer.prototype.run = function( callback ) {
+  if ( !this.options.runner.url ) {
+    callback( ' -> No url to run against defined <- ' );
+
+    return;
+  }
+
+  if (
+    !this.options.runner.allowedDomains ||
+    !this.options.runner.allowedDomains.length
+  ) {
+    callback( ' -> No allowed domains set <- ' );
+
+    return;
+  }
+
+  console.log( chalk.bgBlue.white.bold( ' (╯°□°)╯  Starting to bounce ' ) );
+  console.log( chalk.bgWhite.black( ' Set WPT server: ' ) );
+  console.log( ' -> ' + this.options.wpt.server + '\n' );
+
+  ( new Runner( this.options ) ).run( function( err, data ) {
+    if ( err ) {
+      callback( err );
+
+      return;
+    }
+
+    callback( null, data );
+  } );
+};
+
+
+module.exports = Bouncer;
