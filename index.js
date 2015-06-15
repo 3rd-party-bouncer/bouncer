6297bbda7f4c6af2a2a2dd1423d9da3597f25a60
stefan judis <stefanjudis@gmail.com>
2015-06-16 00:09:47 +0200

    add debug events and clean up a little

--- index.js
+++ index.js
@@ -7,19 +7,22 @@
 
 'use strict';
 
-var Runner = require( './lib/runner' );
-var chalk  = require( 'chalk' );
-var debug  = require( 'debug' )( 'bouncer' );
+var Runner       = require( './lib/runner' );
+var chalk        = require( 'chalk' );
+var EventEmitter = require( 'events' ).EventEmitter;
+var util         = require( 'util' );
 
+/**
+ * Constructor
+ * @param {Object} options options
+ */
 var Bouncer = function( options ) {
-  debug( 'Initialized Bouncer' );
-
   this.options = {
     runner : {
-      url            : options.url,
       allowedDomains : options.allowedDomains,
+      debug          : options.debug,
+      url            : options.url,
       log            : options.log || function( message ) {
-        console.log( '****************************************' );
         console.log( message );
       }
     },
@@ -36,35 +39,50 @@ var Bouncer = function( options ) {
   this.runner = new Runner( this.options );
 };
 
+util.inherits( Bouncer, EventEmitter );
+
+/**
+ * Kick of the bouncer
+ *
+ * @param  {Function} callback callback
+ */
 Bouncer.prototype.run = function( callback ) {
   if ( !this.options.runner.url ) {
-    return callback( ' -> No url to run against defined <- ' );
+    return callback( new Error( ' -> No url to run against defined <- ' ) );
   }
 
   if (
     !this.options.runner.allowedDomains ||
     !this.options.runner.allowedDomains.length
   ) {
-    return callback( ' -> No allowed domains set <- ' );
+    return callback( new Error( ' -> No allowed domains set <- ' ) );
   }
 
-  this.options.runner.log( ' (╯°□°)╯  Starting to bounce ' );
-  this.options.runner.log( ' Set WPT server: ' );
-  this.options.runner.log( ' -> ' + this.options.wpt.server + '\n' );
+  this.options.runner.log( chalk.green( '(╯°□°)╯ ' ) + 'Starting to bounce ' );
+  this.options.runner.log( 'Set WPT server -> ' + this.options.wpt.server );
+  this.options.runner.log( 'Set URL -> ' + this.options.runner.url );
 
+
+  // TODO move these into executable
   this.runner.on( 'error', function( error ) {
-    this.options.runner.log( 'ERROR ->' +  error );
+    this.options.runner.log( chalk.red( 'ERROR' ) +  error );
   }.bind( this ) );
 
   this.runner.on( 'evaluated3rdParties', function( thirdParties ) {
     this.options.runner.log(
-      'EVALUATED 3RD PARTIES ->' + thirdParties.join( ', ' )
+      'Evaluated 3rd parties -> ' + thirdParties.join( ', ' )
     );
   }.bind( this ) );
 
   this.runner.on( 'msg', function( msg ) {
-    this.options.runner.log( msg );
+    this.options.runner.log( chalk.yellow( '(╯°□°)╯ ' ) + msg );
   }.bind( this ) );
+
+  if ( this.options.debug ) {
+    this.runner.on( 'debug', function( msg ) {
+      this.options.runner.log( chalk.red( 'DEBUG:' ) + msg );
+    }.bind( this ) );
+  }
 
   this.runner.run( function( err, data ) {
     if ( err ) {
